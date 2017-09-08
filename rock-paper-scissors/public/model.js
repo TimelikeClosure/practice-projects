2b7d93952f5cfc5c0df0091a90950e293b23530c
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-09-08 14:30:26 -0700

    rps - added user and computer moves to state

--- rock-paper-scissors/public/model.js
+++ rock-paper-scissors/public/model.js
@@ -12,6 +12,8 @@
  *  10 - Paper
  *  11 - Scissors
  */
+const USER_MASK = 1 << 1 | 1;
+const COMP_MASK = USER_MASK << 2;
 
 /**
  * Round state is represented by 2 bits:
@@ -20,15 +22,65 @@
  *  10 - Player 2 won round
  *  11 - Players 1 & 2 tied
  */
+const ROUND_MASK = 1 << 1 || 1 << 4;
+const STATE_MASK = USER_MASK | COMP_MASK | ROUND_MASK;
 
 var rpsState = 0;
 
 function nextRound(){
-
+    rpsState = 0;
 }
 
 function processRound(userMove){
+    //  Stop if game in-play
+    if (rpsState & STATE_MASK) return;
 
+    //  Apply user's move to state
+    rpsState |= userMove;
+
+    //  Generate computer's move
+    let compMove = 1 << 1 | 1;
+    const MOD_3_MASK = 1 << 3;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;  //  1
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;  //  11
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;  //  21
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;  //  31
+    compMove <<= Math.round(Math.random()); compMove |= (compMove & MOD_3_MASK) >> 3; compMove &= ~MOD_3_MASK;
+    //console.log(compMove << 2) & COMP_MASK;
+
+    //  Apply computer's move to state
+    rpsState |= (compMove << 2) & COMP_MASK;
+
+    //  Generate new round status
+
+    //  Apply round status to state
 }
 
 if ((!this.hasOwnProperty('Window') || !(this instanceof this.Window)) && typeof module === "object" && module !== null){
--- rock-paper-scissors/test.js
+++ rock-paper-scissors/test.js
@@ -1,3 +1,39 @@
 "use strict";
 const rps = require('./public/model');
 
+const USER_MASK = 1 << 1 | 1 << 1 | 1;
+const COMP_MASK = USER_MASK << 2;
+const ROUND_MASK = 1 << 1 || 1 << 4;
+
+console.log("===ROUND START===");
+
+console.log("user's state: ", rps.getState() & USER_MASK);
+console.log("comp's state: ", (rps.getState() & COMP_MASK) >>> 2);
+console.log("round state: ", (rps.getState() & ROUND_MASK) >>> 4);
+
+rps.move(3);
+
+console.log("===ROUND FINISH===");
+
+console.log("user's state: ", rps.getState() & USER_MASK);
+console.log("comp's state: ", (rps.getState() & COMP_MASK) >>> 2);
+console.log("round state: ", (rps.getState() & ROUND_MASK) >>> 4);
+
+rps.reset();
+
+console.log("===ROUND RESET===");
+
+console.log("user's state: ", rps.getState() & USER_MASK);
+console.log("comp's state: ", (rps.getState() & COMP_MASK) >>> 2);
+console.log("round state: ", (rps.getState() & ROUND_MASK) >>> 4);
+
+const counts = {};
+
+for (let i = 0, value; i < 10000; i++){
+    rps.move(3);
+    value = (rps.getState() & COMP_MASK) >>> 2;
+    counts[value] = counts.hasOwnProperty(value) ? (counts[value] + 1) : 1;
+    rps.reset();
+}
+
+console.log(counts);
